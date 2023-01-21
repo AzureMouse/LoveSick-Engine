2ad2e88782dd48cd4b9486f066d99db379cf013c
AzureMouse <117840908+AzureMouse@users.noreply.github.com>
2023-01-21 16:56:59 -0500

    Add files via upload
    Added dialog conversations with a player and text.

--- main.py
+++ main.py
@@ -10,18 +10,28 @@ Description:
 import pygame
 from window import *
 from image import *
-
+from text import *
 # Initalize pygame
 pygame.init()
 
 # Various instances
 image_instance = Image()
 win_instance = Window()
+text_instance = Text()
 
 # Colors
 BLACK = 0, 0, 0
+text = "... I got to meet you again. Welcome back."
 
 image_instance.set_background("background.jpg")
+text_instance.set_current_player("Marie" + ":") # Set the current player
+
+text_instance.set_dialog(text) # -> Set the dialog
+
+current_character = text_instance.get_current_player() # -> Fetch current player
+message = text_instance.get_dialog()
+
+font = text_instance.get_font() # Fetch the font
 
 window = pygame.display.set_mode((win_instance.get_width(), win_instance.get_height()))
 
@@ -30,10 +40,14 @@ is_running = True
 background = image_instance.get_background()
 new_size = pygame.transform.scale(background, (win_instance.get_width(), win_instance.get_height()))
 
+character = text_instance.get_current_player()
+
 # Draw
 def draw():
-    window.fill(BLACK)
     window.blit(new_size, (0, 0))
+    text_instance.render_player(window, current_character)
+    text_instance.render_text(window, message) 
+    
     pygame.display.flip()
 
 while(is_running):
@@ -42,3 +56,5 @@ while(is_running):
             is_running = False
         
         draw()
+
+        pygame.display.flip()
--- text.py
+++ text.py
@@ -6,6 +6,57 @@ Author:     AzureMouse
 Description:
 Handles the text of the games
 """
+import pygame
 
 class Text:
 
+    def __init__(self, text_counter=0, dialog=None, player="Zero", current_player=None):
+        self.text_counter = text_counter
+        self.dialog = dialog
+        self.font = pygame.font.SysFont('arialblack', 32)
+        self.player = player
+        self.player_x = 200
+        self.player_y = 550
+        self.text_x = 300
+        self.text_y = 600
+
+    # Setters
+    def set_text_counter(self, value):
+        self.text_counter = value
+
+    def set_dialog(self, value):
+        self.dialog = value
+
+    def set_font(self, value):
+        self.font = value
+
+    def set_current_player(self, player):
+        self.current_player = player
+
+    # Getters
+    def get_text_counter(self):
+        return self.text_counter
+
+    def get_dialog(self):
+        return self.dialog
+
+    def get_font(self):
+        return self.font
+
+    def get_player_name(self):
+        return self.player
+
+    def get_current_player(self):
+        return self.current_player
+    
+    # Render current character speaking
+    def render_player(self, window, player):
+        font = self.font
+        message = font.render(player, False, (255, 255, 255))
+        window.blit(message, (self.player_x, self.player_y))
+
+    # Render current dialog for that character
+    def render_text(self, window, text):
+        font = self.font
+        message = font.render(text, False, (255, 255, 255))
+        window.blit(message, (self.text_x, self.text_y))
